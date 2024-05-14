dba914e8e991a3259286edc8d711b100a646ae2c
j-plou <jesusplou@gmail.com>
2024-05-14 15:41:15 +0200

    Add articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    get "/articles", to: "articles#index"
-  end
+  get "/articles", to: "articles#index"
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -10,4 +8,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "articles#index"
 end
